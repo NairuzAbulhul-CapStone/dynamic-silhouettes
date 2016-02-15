7e5816ba97ba442f202772e6dd3d60bde617f038
Nairuz.Abulhul <nairuz.abulhul@gmail.com>
2016-02-14 16:22:29 -0800

    Update project_intro.js

--- javascripts/project_intro.js
+++ javascripts/project_intro.js
@@ -1,111 +1,162 @@
 $(document).ready(function() {
 
+  //Link to all of the json files
+  var videoLinks1 = ['https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile1.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile2.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile3.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile4.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile5.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile6.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile7.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile8.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile9.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile10.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile11.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile12.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile13.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile14.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile15.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile16.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile17.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile18.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile19.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile20.json'
+  ];
+
+   var videoLinks2 = ['https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile1.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile2.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile3.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile4.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile5.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile6.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile7.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile8.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile9.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile10.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile11.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile12.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile13.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile14.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile15.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile16.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile17.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile18.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile19.json',
+    'https://zeemax.github.io/Amazon_mechanical_turk_project/videoFile20.json'
+  ];
+  
+  var randomize_jsonFiles1 = Math.floor(Math.random() * videoLinks1.length);
+  var randomize_jsonFiles2 = Math.floor(Math.random() * videoLinks2.length);
+
+  var $questionNumber = $("#questionNum");
+  
+
   //Hide next and pre buttons
   $("#next").hide();
   $("#pre").hide();
   $(".videoContent").hide();
   $("#experimentIntroduction").show();
-  // nextVideo();
-  // nextVideo1();
-  //span 
-  var $questionNumber = $("#questionNum");
-  //Click button start to begin the experiment
+  
+  
 
+
+  //A function that show the random files
+  function showVideos1() {
+
+    var vidIndex = parseInt(localStorage.getItem("vidIndex"));
+
+    $.getJSON(videoLinks1[randomize_jsonFiles1], function(videos) {
+
+      var videoFrame = '<iframe embed-responsive src="{{SRC}}?autoplay=1&rel=0" frameborder="0" allowfullscreen></iframe>';
+
+      var randomNumber = Math.floor(Math.random() * 19);
+
+      if (vidIndex) {
+        if (vidIndex >= videoLinks1.length) {
+          vidIndex = 0;
+        }
+
+      } else {
+        if (!vidIndex && vidIndex !== 0) {
+          vidIndex = 0;
+        }
+      }
+
+      var currentVid = videoFrame.replace('{{SRC}}', videos[randomNumber].link);
+      //alert(videos[0].link);
+     $('.content').html(currentVid);
+      $('#vidIndex').val(vidIndex);
+      vidIndex++;
+      localStorage.setItem('vidIndex', vidIndex);
+
+    });
+
+  }
+  
+  
+ 
+  //A function that call back videoLinks2 
+  function showVideos2() {
+
+    var vidIndex = parseInt(localStorage.getItem("vidIndex"));
+
+    $.getJSON(videoLinks2[randomize_jsonFiles2], function(videos) {
+
+      var videoFrame = '<iframe embed-responsive src="{{SRC}}?autoplay=1&rel=0" frameborder="0" allowfullscreen></iframe>';
+
+      var randomNumber = Math.floor(Math.random() * 19);
+
+      if (vidIndex) {
+        if (vidIndex >= videoLinks2.length) {
+          vidIndex = 0;
+        }
+
+      } else {
+        if (!vidIndex && vidIndex !== 0) {
+          vidIndex = 0;
+        }
+      }
+
+      var currentVid = videoFrame.replace('{{SRC}}', videos[randomNumber].link);
+      //alert(videos[0].link);
+      $('.content1').html(currentVid);
+      $('#vidIndex').val(vidIndex);
+      vidIndex++;
+      localStorage.setItem('vidIndex', vidIndex);
+
+    });
+
+  }
+ 
+  
+  
+
+  //START Button
   $("#start").click(function(e) {
-
     $("#experimentIntroduction").hide();
     $(".videoContent").show();
-    nextVideo();
-    nextVideo1();
+    showVideos1();
+    showVideos2();
     $("#next").show();
     $("#pre").show();
 
   });
 
-  //Call functions to move the video
+  //NEXT button
+    $("#next").click(function(e) {
+      showVideos1();
+      showVideos2();
+      $("#intro").hide();
+      
+      if ($questionNumber.text() < 10) {
 
-  $("#next").click(function(e) {
-
-    nextVideo();
-    nextVideo1();
-    $("#intro").hide();
-  });
-
-  function nextVideo() {
-
-    var vidIndex = parseInt(localStorage.getItem("vidIndex"));
-
-    var vids = ['https://www.youtube.com/embed/wSoTDjZGVBs',
-      'https://www.youtube.com/embed/w9pm0vsvPoY',
-      'https://www.youtube.com/embed/hdfcYIxzzFc',
-       'https://www.youtube.com/embed/pdJc5nwn29I',
-        'https://www.youtube.com/embed/aSSc3J2Pj_c',
-         'https://www.youtube.com/embed/qR-qImAv3cY',
-          'https://www.youtube.com/embed/bTTjI2Glaj8',
-           'https://www.youtube.com/embed/1RZaTFuVKzg'
-    ];
-
-    var vidToken = '<iframe embed-responsive src="{{SRC}}" frameborder="0" allowfullscreen></iframe>';
-
-    if (vidIndex) {
-      if (vidIndex >= vids.length) {
-        vidIndex = 0;
+        $questionNumber.text(+$questionNumber.text() + 1);
       }
 
-    } else {
-      if (!vidIndex && vidIndex !== 0) {
-        vidIndex = 0;
-      }
-    }
+    });
 
-    var currentVid = vidToken.replace('{{SRC}}', vids[vidIndex]);
-    $('.content').html(currentVid);
-    $('#vidIndex').val(vidIndex);
-    vidIndex++;
-    localStorage.setItem('vidIndex', vidIndex);
+  
+ 
 
-    //Adding one to the questionNum
-    if ($questionNumber.text() < 10) {
-
-      $questionNumber.text(+$questionNumber.text() + 1);
-    }
-
-  }
-
-  //Second Video Function
-
-  function nextVideo1() {
-
-    var vids = ['https://www.youtube.com/embed/1gXW8nUjGek',
-      'https://www.youtube.com/embed/u-TivFYh9Hg',
-      'https://www.youtube.com/embed/ixk_lqgyhrU',        'https://www.youtube.com/embed/wyWKaFw7GLs',
-      'https://www.youtube.com/embed/EcHyoJ7IoZs',
-      'https://www.youtube.com/embed/FYf1dic8iY4',
-        'https://www.youtube.com/embed/wecue5qdckk',
-         'https://www.youtube.com/embed/9co3JzUWZ_w'
-    ];
-
-    var vidIndex = parseInt(localStorage.getItem("vidIndex"));
-
-    var vidToken = '<iframe embed-responsive src="{{SRC}}" frameborder="0" allowfullscreen></iframe>';
-
-    if (vidIndex) {
-      if (vidIndex >= vids.length) {
-        vidIndex = 0;
-      }
-
-    } else {
-      if (!vidIndex && vidIndex !== 0) {
-        vidIndex = 0;
-      }
-    }
-
-    var currentVid = vidToken.replace('{{SRC}}', vids[vidIndex]);
-    $('.content1').html(currentVid);
-    $('#vidIndex').val(vidIndex);
-    vidIndex++;
-    localStorage.setItem('vidIndex', vidIndex);
-
-  }
 
 });
