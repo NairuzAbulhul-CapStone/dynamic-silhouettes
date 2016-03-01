3fed0d33e69d2a9e024fb05ec67c6e926f77863a
Nairuz.Abulhul <nairuz.abulhul@gmail.com>
2016-03-01 09:39:31 -0800

    Update demo_page.js

--- javascripts/demo_page.js
+++ javascripts/demo_page.js
@@ -97,7 +97,8 @@ $(document).ready(function() {
    material_A_Less_stiffer();
    $("#next").text("Start").css("color","red").css("background-color","white");
    $("#next").click(function(e){
-      window.open("quiz_page.html"); //
+      // window.open("quiz_page.html"); //
+        window.open("http://zeemax.github.io/Amazon_mechanical_turk_project/quiz_page.html","_self");
     });
     
   });
