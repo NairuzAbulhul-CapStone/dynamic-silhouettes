772c50463e3a69d5c66d86d05205d4e83ca3ad9c
Nairuz.Abulhul <nairuz.abulhul@gmail.com>
2016-03-01 09:36:52 -0800

    Update demo_page.js

--- javascripts/demo_page.js
+++ javascripts/demo_page.js
@@ -97,7 +97,7 @@ $(document).ready(function() {
    material_A_Less_stiffer();
    $("#next").text("Start").css("color","red").css("background-color","white");
    $("#next").click(function(e){
-      window.open("quiz_page.html");
+      window.open("quiz_page.html"); //
     });
     
   });
